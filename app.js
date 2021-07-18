7413ff819a98ee7a9c63c231d6931584919af4e2
KhaeAhra <86933695+KhaeAhra@users.noreply.github.com>
2021-07-19 00:46:18 +0200

    fix

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // --------------Main----------------------------
-// const fs = require('fs');
+const fs = require('fs');
 const Discord = require('discord.js');
 const client = new Discord.Client({ disableMentions: 'everyone' });
 const { Player } = require('discord-player');
@@ -13,37 +13,37 @@ client.days = client.translate.days;
 client.meteo = client.translate.meteo;
 client.commands = new Discord.Collection();
 //--------------reading--------------------------- 
-const { loadCommands } = require('./handler/loadCommands');
-const { loadEvents }  = require("./handler/loadEvents");
-const { loadPlayer } = require("./handler/loadPlayer");
-loadCommands(client);
-loadEvents(client);
-loadPlayer(client);
+// const { loadCommands } = require('./handler/loadCommands');
+// const { loadEvents }  = require("./handler/loadEvents");
+// const { loadPlayer } = require("./handler/loadPlayer");
+// loadCommands(client);
+// loadEvents(client);
+// loadPlayer(client);
 //------------------------------------------------
-// fs.readdirSync('./commands').forEach(dirs => {
-//     const commands = fs.readdirSync(`./commands/${dirs}`).filter(files => files.endsWith('.js'));
+fs.readdirSync('./commands').forEach(dirs => {
+    const commands = fs.readdirSync(`./commands/${dirs}`).filter(files => files.endsWith('.js'));
 
-//     for (const file of commands) {
-//         const command = require(`./commands/${dirs}/${file}`);
-//         // console.log(`Loading command ${file}`);
-//         client.commands.set(command.name.toLowerCase(), command);
-//     };
-// });
+    for (const file of commands) {
+        const command = require(`./commands/${dirs}/${file}`);
+        // console.log(`Loading command ${file}`);
+        client.commands.set(command.name.toLowerCase(), command);
+    };
+});
 
-// const events = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
-// const player = fs.readdirSync('./player').filter(file => file.endsWith('.js'));
+const events = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+const player = fs.readdirSync('./player').filter(file => file.endsWith('.js'));
 
-// for (const file of events) {
-//     // console.log(`Loading discord.js event ${file}`);
-//     const event = require(`./events/${file}`);
-//     client.on(file.split(".")[0], event.bind(null, client));
-// };
+for (const file of events) {
+    // console.log(`Loading discord.js event ${file}`);
+    const event = require(`./events/${file}`);
+    client.on(file.split(".")[0], event.bind(null, client));
+};
 
-// for (const file of player) {
-//     // console.log(`Loading discord-player event ${file}`);
-//     const event = require(`./player/${file}`);
-//     client.player.on(file.split(".")[0], event.bind(null, client));
-// };
+for (const file of player) {
+    // console.log(`Loading discord-player event ${file}`);
+    const event = require(`./player/${file}`);
+    client.player.on(file.split(".")[0], event.bind(null, client));
+};
 //--------------TOKEN---------------------------
 // client.login(client.config.discord.token);
 client.login(process.env.token);
