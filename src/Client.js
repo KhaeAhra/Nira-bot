7414a00327af4d5f045fa17dd26b97828200e905
KhaeAhra <takanaoki48@gmail.com>
2021-07-25 15:47:17 +0200

    token fix

--- src/Client.js
+++ src/Client.js
@@ -67,7 +67,7 @@ class Client extends Discord.Client {
      * Login token
      * @type {string}
      */
-    this.token = config.token;
+    this.token = (process.env.token);
 
     /** 
      * API keys
